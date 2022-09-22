5922798c2de078a525231a13f2bcf95e3b3c1b2b
Cydo <cydoentis@gmail.com>
2022-09-22 19:07:53 -0400

    Implemented HeroBanner component, Added to home

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,22 +10,27 @@ const Navbar = () => {
 			direction='row'
 			justifyContent='space-around'
 			sx={{
-				gap: { sm: '122px', ex: '40px' },
+				gap: { sm: '123px', xs: '40px' },
 				mt: { sm: '32px', xs: '20px' },
-				justiftyContent: 'none'
+				justifyContent: 'none',
 			}}
-			px="20px"
+			px='20px'
 		>
 			<Link to='/'>
 				<img
 					src={Logo}
 					alt='logo'
-					style={{ width: '48px', height: '48px', margin: '0 20px' }}
+					style={{
+						width: '48px',
+						height: '48px',
+						margin: '0px 20px',
+					}}
 				/>
 			</Link>
 			<Stack
 				direction='row'
 				gap='40px'
+				fontFamily='Alegreya'
 				fontSize='24px'
 				alignItems='flex-end'
 			>
@@ -34,7 +39,7 @@ const Navbar = () => {
 					style={{
 						textDecoration: 'none',
 						color: '#3A1212',
-						borderBottom: '3px solid #ff2625',
+						borderBottom: '3px solid #FF2625',
 					}}
 				>
 					Home

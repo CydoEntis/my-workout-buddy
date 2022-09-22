0f9f00ef03fa7e7ee69560d6a9911b7c7643238a
Cydo <cydoentis@gmail.com>
2022-09-22 18:48:05 -0400

    Implemented Navbar, and fixed dependencies

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,16 @@ import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => {
 	return (
-		<Stack>
+		<Stack
+			direction='row'
+			justifyContent='space-around'
+			sx={{
+				gap: { sm: '122px', ex: '40px' },
+				mt: { sm: '32px', xs: '20px' },
+				justiftyContent: 'none'
+			}}
+			px="20px"
+		>
 			<Link to='/'>
 				<img
 					src={Logo}
@@ -14,10 +23,29 @@ const Navbar = () => {
 					style={{ width: '48px', height: '48px', margin: '0 20px' }}
 				/>
 			</Link>
-      <Stack>
-        <Link to='/'>Home</Link>
-        <a href="#exercises" style={{textDecoration: 'none', color: '#3A1212'}}></a>
-      </Stack>
+			<Stack
+				direction='row'
+				gap='40px'
+				fontSize='24px'
+				alignItems='flex-end'
+			>
+				<Link
+					to='/'
+					style={{
+						textDecoration: 'none',
+						color: '#3A1212',
+						borderBottom: '3px solid #ff2625',
+					}}
+				>
+					Home
+				</Link>
+				<a
+					href='#exercises'
+					style={{ textDecoration: 'none', color: '#3A1212' }}
+				>
+					Exercises
+				</a>
+			</Stack>
 		</Stack>
 	);
 };

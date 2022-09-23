75bbd3621846e375b116bf202e71fc2153f70353
Cydo <cydoentis@gmail.com>
2022-09-23 19:46:01 -0400

    Implemented Exercise Videos

--- client/src/pages/ExerciseDetail.jsx
+++ client/src/pages/ExerciseDetail.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
+
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 
 const ExerciseDetail = () => {
-  return (
-    <div>ExerciseDetail</div>
-  )
-}
+	const [exerciseDetail, setExerciseDetail] = useState({});
+	const [exerciseVideos, setExerciseVideos] = useState([]);
+	const { id } = useParams();
 
-export default ExerciseDetail
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+			const youtubeSearchUrl =
+				'https://youtube-search-and-download.p.rapidapi.com';
+
+			const exerciseDetailData = await fetchData(
+				`${exerciseDbUrl}/exercises/exercise/${id}`,
+				exerciseOptions
+			);
+
+			const exerciseVideosData = await fetchData(
+				`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,
+				youtubeOptions
+			);
+
+			setExerciseDetail(exerciseDetailData);
+			setExerciseVideos(exerciseVideosData.contents);
+		};
+
+		fetchExercisesData();
+	}, [id]);
+
+	return (
+		<Box>
+			<Detail exerciseDetail={exerciseDetail} />
+			<ExerciseVideos
+				exerciseVideos={exerciseVideos}
+				name={exerciseDetail.name}
+			/>
+			<SimilarExercises />
+		</Box>
+	);
+};
+
+export default ExerciseDetail;
